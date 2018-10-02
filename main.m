ff7541be96b2e04192b46e785a226bb923c109b2
smarty1palak <smarty1palak@gmail.com>
2018-10-02 14:49:39 +0530

    input in prime number format

--- main.m
+++ main.m
@@ -14,23 +14,66 @@ fprintf('\nTime before computation:\t')
 fprintf(tbc)
 fprintf('\n\n')
 
-for p=2:10
-flag=0;
-for x=2:p
-    x
-    if mod(x,p)~=0
-    lhs=x^(p-1)
-    remainder=mod(lhs,p)
-     if(remainder~=1)
-        flag=1
-        fprintf('\n%d is not prime\n\n', p)
-        break
-     end
-    else
-        fprintf('%d is not coprime to prime: %d\n', x, p)
+
+prompt = 'What is the original value? ';
+inp_string = input(prompt, 's');
+if isempty(strfind(inp_string, '-'))
+  idx= strfind(inp_string, '+')
+  sign = '+'
+else
+  idx= strfind(inp_string, '-')
+  sign = '-'
+end
+
+astr= substr( inp_string, 1, idx-2)
+bstr= substr(inp_string, idx+1, length(inp_string)-idx)
+
+a=str2num(astr)
+b=str2num(bstr)
+
+
+primes=[];
+for n=1:10
+  if sign =='-'
+    number = a*n -b;
+  else
+    number = a*n +b;
+  end
+  
+  if number<0
+    continue;
+  end
+  
+  p=number
+  
+  flag=1;
+  for a=1:p
+    if gcd(p,a)~=1
+        continue;
     end
-end 
-if flag==0 
- fprintf('\n\%d is prime\n\n', p)
+    lhs=pow(a,p-1,p)
+    if lhs ~= 1
+        flag=0;
+    end
+  end
+  
+  if flag==1
+    primes=[primes,p];
+  end
+end
+
+primes
+
+function x=pow(a,n,m)
+b=a;
+x = 1;
+while n>0
+d = rem(n,2);
+if d==1
+x = rem(x*b,m);
+end
+b = rem(b * b,m);
+n = (n-d)/2;
 end
 end
+
