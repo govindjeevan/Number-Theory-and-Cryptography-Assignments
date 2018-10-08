8b916634fb6af581c2b96e48e9287d273673cde7
smarty1palak <smarty1palak@gmail.com>
2018-10-08 23:23:14 +0530

    some cleanup

--- solution4.m
+++ solution4.m
@@ -1,7 +1,20 @@
 N = 1:11400;
-remainder = powermod(4,N-1,N);
+remainder = pow(4,N-1,N);
 primesFermat = N(remainder == 1)
 
 primeNumbers = N(isprime(N));
 setdiff(primesFermat,primeNumbers)
 
+
+function x=pow(a,n,m)
+b=a;
+x = 1;
+while n>0
+d = rem(n,2);
+if d==1
+x = rem(x*b,m);
+end
+b = rem(b * b,m);
+n = (n-d)/2;
+end
+end
